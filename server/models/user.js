4d7f3cf780c2ff137cbf7191b2abcc7470983a03
Gabor Haraszti <gharaszti71@gmail.com>
2019-05-05 17:33:50 +0200

    User management

--- server/models/user.js
+++ server/models/user.js
@@ -12,17 +12,27 @@ class User {
     constructor(name, type, password) {
         this.id = uuid()
         this.name = name.trim()
+        if (type !== UserType.Admin && type !== UserType.Service) {
+            throw new Error('Invalid user type!')
+        }
         this.type = type
         this.password = bcrypt.hashSync(password, 12)
+        this.sessions = []
     }
 
     // id : uuid
     // name : String
     // type : UserType
     // password : salt + password hash, String
-
-    static async Login(name, password) {
-        const user = users.find(u => u.name === name)
+    /**
+     * 
+     * @param {String} name Felhasználói név
+     * @param {String} password Felhasználói jelszó (plaintext)
+     * @param {UserType} type Felhasználó tipus (Admin/Service)
+     * @returns {String} JSON Web token
+     */
+    static async login(name, password, type) {
+        const user = users.find(u => u.name === name && u.type === type)
         if (!user || !await bcrypt.compare(password, user.password)) {
             throw new Error('Invalid credentials!')
         }
@@ -34,13 +44,13 @@ class User {
     /**
      * Felhasználók betöltése a config/users.json fájlból a memóriába
      */
-    static async Load() {
+    static async load() {
         return new Promise((resolve, reject) => {
             fs.readFile(configFilePath, async (err, data) => {
                 if (err) {
                     users = []
-                    await User.Add('admin',UserType.Admin, 'password')
-                    resolve()
+                    await User.add('admin',UserType.Admin, 'password')
+                    return resolve()
                 }
                 users = JSON.parse(data.toString())
                 resolve()
@@ -51,7 +61,7 @@ class User {
     /**
      * Felhasználók elmentése. Hiba esetén továbbadjuk azt!
      */
-    static async Save() {
+    static async save() {
         return new Promise((resolve, reject) => {
             const usersToSave = JSON.stringify(users)
             fs.writeFile(configFilePath, usersToSave, (error) => {
@@ -69,26 +79,51 @@ class User {
      * @param {UserType} type Felhasználó típusa: Admin/Service
      * @param {String} password Felhasználó jelszava
      */
-    static async Add(name, type, password) {
+    static async add(name, type, password) {
+        if (users.some(u => u.name === name)) {
+            return
+        }
         const user = new User(name, type, password)
         users.push(user)
-        await User.Save()
+        await User.save()
     }
 
     /**
      * Felhasználó törlése
      * @param {uuid} id Felhasználó azonosítója
      */
-    static async Remove(id) {
+    static async remove(id) {
         const usersKeep = users.filter(u => u.id !== id)
         if (usersKeep.length !== users.length) {
-            await User.Save()
+            users = usersKeep
+            await User.save()
         }
+    }
+
+    /**
+     * Betölti a felhasználót az id alapján
+     * @param {uuid} id Felhasználói egyedi azonosító
+     */
+    static async get(id) {
+        const user = users.find(u => u.id === id)
+        if (!user) {
+            throw new Error('User not found!')
+        }
+        return user;
+    }
+
+    /**
+     * Visszaadja valamennyi felhasználót
+     */
+    static async getAll() {
+        return new Promise((resolve, reject) => {
+            resolve(users)
+        })
     }
 }
 
 // Load users from disc
-User.Load().then(() => {
+User.load().then(() => {
     console.log('Users loaded at', new Date().toTimeString())
 })
 
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,9 +1,45 @@
 const express = require('express')
 const User = require('../models/user')
+const UserType = require('../models/userType')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
+/**
+ * Admin User login
+ */
+router.post('/users/login', async (req, res) => {
+    try {
+        const token = await User.login(req.body.name, req.body.password, UserType.Admin)
+        res.send({ token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 
+/**
+ * Create User
+ */
+router.post('/users', auth, async (req, res) => {
+    const userType = req.body.type === 'Admin' ? UserType.Admin : req.body.type === 'Service' ? UserType.Service : undefined
+    try {
+        await User.add(req.body.name, userType, req.body.password)
+        res.status(201).send()
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
+/**
+ * Get full user list
+ */
+router.get('/users', auth, async (req, res) => {
+    try {
+        const users = await User.getAll()
+        res.send(users)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 
 module.exports = router
