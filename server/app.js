6d92d24303405fe75283b33feb3c138e5633021c
Gabor Haraszti <gharaszti71@gmail.com>
2019-05-20 21:55:47 +0200

    Add frontend with AngularJS 7

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 "use strict";
 
+const path = require('path')
 const express = require('express')
 const userRouter = require('./routers/user')
 const serviceRouter = require('./routers/service')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
 
+app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 app.use(userRouter)
 app.use(serviceRouter)
